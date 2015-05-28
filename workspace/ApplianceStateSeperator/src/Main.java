a1255af1648d44971ddf10a425eff3e95ab4a84a
Marius Wodtke <Marius.Wodtke@Exxeta.de>
2015-05-28 12:11:52 +0200

    Weka eingebunden
    Weka in den ApplianceStateSeperator eingebunden und erfolgreich getestet

--- workspace/ApplianceStateSeperator/src/Main.java
+++ workspace/ApplianceStateSeperator/src/Main.java
@@ -1,12 +1,23 @@
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 import Features.*;
 import Vectors.*;
+
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.classifiers.meta.FilteredClassifier;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.Evaluation;
+
 /**
  * 
  */
@@ -25,11 +36,12 @@ public class Main {
 		DBConnect db = new DBConnect("jdbc:mysql://localhost:3306/testdatenbank?" +
                 "user=root");
 		
+		
 		ResultSet result = db.getSQL("SELECT * FROM `training` ORDER BY `Unixtime`");
 		
 		try {
 			
-			result.first();
+			//result.first();
 			/*
 			 * System.out.println(result.getString("Wirkleistung"));
 			result.next();
@@ -56,7 +68,7 @@ public class Main {
 			//Expander ex = new Expander(db);
 			//expand profil0
 			//ex.expand("SELECT * FROM `profil8` ORDER BY `Unixtime`", "INSERT INTO profil8training (`Unixtime`,`Reltime`,`Spannung`,`Stromstärke`,`Wirkleistung`, `Klasse`)", 1384066710, 5550);
-			FeatureWriter fw = new FeatureWriter(db, false);
+			//FeatureWriter fw = new FeatureWriter(db, false);
 			/*fw.zeitreihe("SELECT * FROM `training` ORDER BY `Unixtime`", "INSERT INTO `trainingzeitnorm` (`Unixtime`, "
 					+ "`Wirkleistung t-10`, `Wirkleistung t-9`, `Wirkleistung t-8`, `Wirkleistung t-7`, `Wirkleistung t-6`, "
 					+ "`Wirkleistung t-5`, `Wirkleistung t-4`, `Wirkleistung t-3`, `Wirkleistung t-2`, `Wirkleistung t-1`, "
@@ -64,8 +76,43 @@ public class Main {
 					+ "`Wirkleistung t+5`, `Wirkleistung t+6`, `Wirkleistung t+7`, `Wirkleistung t+8`, `Wirkleistung t+9`, "
 					+ "`Wirkleistung t+10`, `Klasse`)", 1382290611, 1384072259);
 			*/
-			fw.experte("SELECT * FROM `training` ORDER BY `Unixtime`", "INSERT INTO `experte` (`Unixtime`, `Wirkleistung`, `FolgeDurchschnitt`, `MaxWirkleistung`, `MinPeakWirkleistung`,`Klasse`)", 1382290611, 1384072259);
-		} catch (SQLException e) {System.out.println(e);}
+			//fw.experte("SELECT * FROM `training` ORDER BY `Unixtime`", "INSERT INTO `experte` (`Unixtime`, `Wirkleistung`, `FolgeDurchschnitt`, `MaxWirkleistung`, `MinPeakWirkleistung`,`Klasse`)", 1382290611, 1384072259);
+			 
+			 DataSource source = new DataSource("C:\\Users\\mawo\\Documents\\GitHub\\Thesis\\workspace\\ApplianceStateSeperator\\src\\trainingszeit.arff");
+			 Instances data = source.getDataSet();
+			 if (data.classIndex() == -1)
+				   data.setClassIndex(data.numAttributes() - 1);
+			 
+			 System.out.println("Successfully loaded Data\n");
+			 
+			 //String[] optionsrm = weka.core.Utils.splitOptions("-R 1");
+			 /*
+			 NumericToNominal ntn = new NumericToNominal();
+			 ntn.setAttributeIndices("last");
+			 ntn.setInputFormat(data);                          
+			 Instances newData = Filter.useFilter(data, ntn);
+			 */
+			 Remove rm = new Remove(); 
+			 rm.setAttributeIndices("1");                        
+			 //remove.setOptions(optionsrm);                           
+			 //remove.setInputFormat(data);  
+			 //Instances newData = Filter.useFilter(data, rm);  
+			 
+			 MultilayerPerceptron mlp = new MultilayerPerceptron();
+			 mlp.setHiddenLayers("t");
+			 mlp.setTrainingTime(5);
+			 
+			 FilteredClassifier fc = new FilteredClassifier();
+			 fc.setFilter(rm);
+			 fc.setClassifier(mlp);
+			 
+			 Evaluation eval = new Evaluation(data);
+			 eval.crossValidateModel(fc, data, 10, new Random(1));
+			 System.out.println(eval.toSummaryString(false));
+			 System.out.println(eval.toClassDetailsString());
+			 System.out.println(eval.toMatrixString());
+			
+		} catch (Exception e) {System.out.println(e);}
 		finally {
 			db.cleanSQL();
 		}
